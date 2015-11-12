042c5bf4e5846f58f128b8a4d505f6ecb48b2550
woxingxiao <185578499@qq.com>
2015-11-12 23:29:53 +0800

    initial commit

---
+++ library/src/main/java/com/repo/xw/library/views/PullListView.java
@@ -0,0 +1,74 @@
+package com.repo.xw.library.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * 支持上下拉刷新的ListView
+ * Created by woxingxiao on 2015/11/12.
+ */
+public class PullListView extends ListView implements Pullable {
+
+    private boolean pullDownEnable = true; //下拉刷新开关
+    private boolean pullUpEnable = true; //上拉刷新开关
+
+    public PullListView(Context context) {
+        super(context);
+    }
+
+    public PullListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PullListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean canPullDown() {
+        if (!pullDownEnable)
+            return false;
+        if (getCount() == 0) {
+            // 没有item的时候也可以下拉刷新
+            return true;
+        } else if (getFirstVisiblePosition() == 0 && getChildAt(0).getTop() >= 0) {
+            // 滑到ListView的顶部了
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean canPullUp() {
+        if (!pullUpEnable)
+            return false;
+        if (getCount() == 0) {
+            // 没有item的时候也可以上拉加载
+            return true;
+        } else if (getLastVisiblePosition() == (getCount() - 1)) {
+            // 滑到底部了
+            if (getChildAt(getLastVisiblePosition() - getFirstVisiblePosition()) != null
+                    && getChildAt(getLastVisiblePosition() - getFirstVisiblePosition()).getBottom() <= getMeasuredHeight())
+                return true;
+        }
+        return false;
+    }
+
+    public boolean isPullDownEnable() {
+        return pullDownEnable;
+    }
+
+    public void setPullDownEnable(boolean pullDownEnable) {
+        this.pullDownEnable = pullDownEnable;
+    }
+
+    public boolean isPullUpEnable() {
+        return pullUpEnable;
+    }
+
+    public void setPullUpEnable(boolean pullUpEnable) {
+        this.pullUpEnable = pullUpEnable;
+    }
+}
